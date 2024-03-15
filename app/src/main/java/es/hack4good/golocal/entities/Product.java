c8dcbb42fab15ca861bb565dade56df0678e6e4b
Pablo R. P <pablorepost@gmail.com>
2024-03-15 13:48:38 +0100

    product and store

---
+++ app/src/main/java/es/hack4good/golocal/entities/Product.java
@@ -0,0 +1,105 @@
+package es.hack4good.golocal.entities;
+
+public class Product {
+    private long id;
+    private String name;
+    private String description;
+    private double price;
+    private ProductType type;
+    private Store store;
+    private String image;
+    private int stock;
+
+    public Product(long id, String name, String description, double price, ProductType type, Store store, String image, int stock) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.type = type;
+        this.store = store;
+        this.image = image;
+        this.stock = stock;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public ProductType getType() {
+        return type;
+    }
+
+    public void setType(ProductType type) {
+        this.type = type;
+    }
+
+    public Store getStore() {
+        return store;
+    }
+
+    public void setStore(Store store) {
+        this.store = store;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public void addStock(int stock) {
+        this.stock += stock;
+    }
+
+    public void removeStock(int stock) {
+        this.stock -= stock;
+    }
+
+    public boolean hasStock(int stock) {
+        return this.stock >= stock;
+    }
+
+    public boolean isType(ProductType type) {
+        return this.type.equals(type);
+    }
+
+}
+
--- app/src/main/java/es/hack4good/golocal/entities/Store.java
+++ app/src/main/java/es/hack4good/golocal/entities/Store.java
@@ -1,6 +1,112 @@
 package es.hack4good.golocal.entities;
 
+import java.util.HashMap;
+
 public class Store {
     private String name;
     private Location location;
+    public HashMap<Long, Product> products;
+    private String image;
+    private String description;
+    private String phone;
+    private String email;
+    private String web;
+    public Store(String name, Location location, String image, String description, String phone, String email, String web) {
+        this.name = name;
+        this.location = location;
+        this.image = image;
+        this.description = description;
+        this.phone = phone;
+        this.email = email;
+        this.web = web;
+        this.products = new HashMap<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public HashMap<Long, Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(HashMap<Long, Product> products) {
+        this.products = products;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getWeb() {
+        return web;
+    }
+
+    public void setWeb(String web) {
+        this.web = web;
+    }
+
+    private boolean addProduct(Product product) {
+        if(products.containsKey(product.getId())) {
+            return false;
+        }
+        products.put(product.getId(), product);
+        return true;
+    }
+
+    private boolean removeProduct(Product product) {
+        if(products.containsKey(product.getId())) {
+            products.remove(product.getId());
+            return true;
+        }
+        return false;
+    }
+
+    private boolean updateProduct(Product product) {
+        if(products.containsKey(product.getId())) {
+            products.put(product.getId(), product);
+            return true;
+        }
+        return false;
+    }
 }
