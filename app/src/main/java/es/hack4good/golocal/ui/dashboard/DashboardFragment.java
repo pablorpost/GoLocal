7a8df05247b3ad9e1b0d2f61e94675774c06960d
miguel <ma.garciam.2021@alumnos.urjc.es>
2024-03-16 11:39:59 +0100

    cesta

--- app/src/main/java/es/hack4good/golocal/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/es/hack4good/golocal/ui/dashboard/DashboardFragment.java
@@ -1,37 +1,51 @@
 package es.hack4good.golocal.ui.dashboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import java.util.List;
+
+import es.hack4good.golocal.ProductAdapter;
+import es.hack4good.golocal.R;
+import es.hack4good.golocal.ShowProduct;
+import es.hack4good.golocal.database.AppDatabase;
+import es.hack4good.golocal.database.entity.Product;
+import es.hack4good.golocal.database.services.DbInitializer;
 import es.hack4good.golocal.databinding.FragmentDashboardBinding;
 
 public class DashboardFragment extends Fragment {
 
-    private FragmentDashboardBinding binding;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        ListView listView = view.findViewById(R.id.listView2);
+        AppDatabase db = AppDatabase.buildDatabaseInstance(getActivity(),"golocal",false);
+        if(db.productDAO().getAllProducts().isEmpty()){
+            DbInitializer.initialize(db);
+        }
+        List<Product> products = db.productDAO().getAllProducts();
+        System.out.println("prueba");
+        System.out.println(products);
+        ProductAdapter adapter = new ProductAdapter(getActivity(),products);
+        listView.setAdapter(adapter);
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+
+
+        return view;
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
+
 }
