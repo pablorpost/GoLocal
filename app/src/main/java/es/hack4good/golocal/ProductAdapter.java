85f4016da24ad0646e1a1851bcfa8aeb424592f8
miguel <ma.garciam.2021@alumnos.urjc.es>
2024-03-16 01:22:46 +0100

    product list xml

---
+++ app/src/main/java/es/hack4good/golocal/ProductAdapter.java
@@ -0,0 +1,47 @@
+package es.hack4good.golocal;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import es.hack4good.golocal.database.entity.Product;
+
+public class ProductAdapter extends BaseAdapter {
+    Context context;
+    ArrayList<Product> products;
+
+    @Override
+    public int getCount() {
+        return products.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        TextView textView;
+
+        Product p = products.get(position);
+
+        if(convertView == null){
+            convertView = LayoutInflater.from(context).inflate(R.layout.shopitem,null);
+        }
+
+        textView = convertView.findViewById(R.id.nombreProducto);
+        textView.setText(p.getName());
+        return null;
+    }
+}
